491d7f10c8b6eecd54c799b51fac4832b495ea65
Zunaid <zunaid.omar@absa.africa>
2020-12-31 09:03:59 +0200

    Update csv_mssql_importer.py

--- csv_mssql_importer.py
+++ csv_mssql_importer.py
@@ -3,18 +3,21 @@ import urllib
 import pandas as pd 
 
 
+db_instance_name = r'ZUNA-LAPTOP'
+db_name = 'DataLakesDB'
+new_db_name = 'Insurance'
 
-
+path = r"C:\Users\T440P\Downloads\People.csv"
 
 
 def Data_Import():
 
    
 
-    quoted = urllib.parse.quote_plus('DRIVER={SQL Server};SERVER=ZUNA-LAPTOP;DATABASE=DataLakesDB;Trusted_Connection=yes')
+    quoted = urllib.parse.quote_plus('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+db_instance_name+';DATABASE='+db_name+';Trusted_Connection=yes')
     engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted))
 
-    df = pd.read_csv(r"C:\Users\T440P\Downloads\People.csv",engine='python',index_col=False)
+    df = pd.read_csv(path,engine='python',index_col=False)
 
     df.to_sql('zuna',schema='dbo',con=engine,if_exists='replace',index=False)
     print('Done with import')
